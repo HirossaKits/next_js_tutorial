0ad9c0816a77afd81d8462ef61328d8b1b12b305
kitsuka <hi.kitsuka@gmail.com>
2021-11-21 12:38:55 +0900

    Inject TypeScript

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,8 +3,17 @@ import Head from "next/head";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
+import { GetStaticProps, GetStaticPaths } from "next";
 
-export default function Post({ postData }) {
+type Props = {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+};
+
+export default function Post({ postData }: Props) {
   return (
     <Layout>
       <Head>
@@ -22,19 +31,36 @@ export default function Post({ postData }) {
   );
 }
 
-export async function getStaticPaths() {
+// export async function getStaticPaths() {
+//   const paths = getAllPostIds();
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
+
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
     fallback: false,
   };
-}
+};
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
+// export async function getStaticProps({ params }) {
+//   const postData = await getPostData(params.id);
+//   return {
+//     props: {
+//       postData,
+//     },
+//   };
+// }
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string);
   return {
     props: {
       postData,
     },
   };
-}
+};
